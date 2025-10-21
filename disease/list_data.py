591ce1acc2a00c14c32ede45f6c6f303883de2fa
TwoTanawin <tanawinsiriwan@outlook.com>
2025-10-21 17:00:35 +0700

    disease ana v2

--- disease/list_data.py
+++ disease/list_data.py
@@ -2,20 +2,30 @@ from .utils.mongo import MongoConnection
 import asyncio
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv()
 
 async def main():
     mongo = MongoConnection(collections=os.getenv("DISEASE_COLLECTION"))
     mongo.connect()
-    collection = mongo.get_db()
+    _, collection = mongo.get_db()
     
-    # Include _id and diseaseLocation
+    results = []
     for doc in collection.find({}, {"diseaseLocation": 1, "_id": 1}):
-        print({
-            "id": str(doc["_id"]),                # convert ObjectId to string
-            "diseaseLocation": doc.get("diseaseLocation")
+        results.append({
+            "id": str(doc["_id"]),  # convert ObjectId to string
+            "latitude": doc.get("diseaseLocation", {}).get("latitude"),
+            "longitude": doc.get("diseaseLocation", {}).get("longitude")
         })
+
+    # Convert to DataFrame
+    df = pd.DataFrame(results)
+
+    # Export to Parquet
+    df.to_parquet("disease_locations.parquet", engine="pyarrow", index=False)
+
+    print("Exported to disease_locations.parquet")
 
 if __name__ == "__main__":
     asyncio.run(main())
